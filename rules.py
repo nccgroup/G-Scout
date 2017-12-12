e33cecb31d56e4f3fe6d8dd36a36503ede9178a3
lgap <34092979+lgap@users.noreply.github.com>
2017-12-12 21:37:37 +0100

    Bug fix: exception handler for Rule class
    This fixes that rule processing continues; even when information is not found/available/rule is incorrect.

--- rules.py
+++ rules.py
@@ -8,9 +8,12 @@ def rules(projectId):
 	db = TinyDB("project_dbs/" + projectId + ".json")
 	class Rule:
 		def __init__(self, rule_title, category, filter_func):
-			db.table('Rule').insert({"title":rule_title,"category":category})
-			for entity in list(filter(filter_func,db.table(category).all())):
-				add_finding.add_finding(db,category, entity.eid, rule_title)
+			try:
+				db.table('Rule').insert({"title":rule_title,"category":category})
+				for entity in list(filter(filter_func,db.table(category).all())):
+					add_finding.add_finding(db,category, entity.eid, rule_title)
+			except KeyError:
+				pass
 
 	Rule("Unused Network", "Network", 
 		lambda network: not network.get('members'))
