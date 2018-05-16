2629bbff93b7ea9dac07dab626ba16b7cd184935
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.trust>
2018-05-16 17:45:26 +0200

    Implement argparse for command line arguments.

--- README.md
+++ README.md
@@ -16,12 +16,13 @@ There are two ways for the project owner to grant API permissions:
 To run the application:
 ```sh
 sudo pip install -r requirements.txt
-python gscout.py "project" "project name"
+python gscout.py -h
 ```
+
 The HTML report output will be in the "Report Output" folder.
 
-When specifying the project name you can also use a wildcard to run G-Scout on multiple projects, for example: `python gscout.py "project" "dev-*"`.
-You can also run G-Scout on all projects in an organization like this: `python gscout.py "organization" "organization id"`, where the id will be a number you can find next to the organization name in the GCP console. 
+When specifying the project name you can also use a wildcard to run G-Scout on multiple projects, for example: `python gscout.py --project "dev-*"`.
+You can also run G-Scout on all projects in an organization like this: `python gscout.py --organization "organization id"`, where the id will be a number you can find next to the organization name in the GCP console. 
 
 To create a custom rule, add it to the rules.py file. A Rule object takes a name, a category, and a filter function. The function will be passed a json object corresponding to the category. 
 To see an example for each category (some of which are altered from the standard API response), see the entity_samples.json file.
--- gscout.py
+++ gscout.py
@@ -3,17 +3,25 @@ from oauth2client.client import ApplicationDefaultCredentialsError
 from oauth2client.client import HttpAccessTokenRefreshError
 from oauth2client.file import Storage
 from tinydb import TinyDB
+import os
+import sys
+from fetch import fetch
+import logging
+import argparse
+
+# configure command line parameters
+parser = argparse.ArgumentParser(description='Google Cloud Platform Security Tool')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--project', '-p', help='Project name to scan')
+group.add_argument('--organization', '-o', help='Organization id to scan')
+args = parser.parse_args()
 
 storage = Storage('creds.data')
-import logging
 
 logging.basicConfig(filename="log.txt")
 logging.getLogger().setLevel(logging.ERROR)
 # Silence some errors
 logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
-import os
-import sys
-from fetch import fetch
 
 try:
     os.remove("projects.json")
@@ -30,16 +38,19 @@ def list_projects(project_or_org, specifier):
         request = service.projects().list(filter='parent.id:%s' % specifier)
     elif project_or_org == "project":
         request = service.projects().list(filter='name:%s' % specifier)
+        # TODO add filtering with project ID
+        # request = service.projects().list(filter='id:%s' % specifier)
     else:
         raise Exception('Organization or Project not specified.')
     while request is not None:
         response = request.execute()
-        for project in response['projects']:
-            if (project['lifecycleState'] != "DELETE_REQUESTED"):
-                db.table('Project').insert(project)
+        if 'projects' in response.keys():
+            for project in response['projects']:
+                if (project['lifecycleState'] != "DELETE_REQUESTED"):
+                    db.table('Project').insert(project)
 
-        request = service.projects(). \
-            list_next(previous_request=request, previous_response=response)
+        request = service.projects().list_next(previous_request=request,
+                                               previous_response=response)
 
 
 def fetch_all(project):
@@ -58,16 +69,24 @@ def fetch_all(project):
         logging.exception(e)
 
 
-try:
-    os.makedirs("Report Output")
-except:
-    pass
-try:
-    list_projects(sys.argv[1], sys.argv[2])
-except (HttpAccessTokenRefreshError, ApplicationDefaultCredentialsError):
-    import config
+def main():
 
-    list_projects(sys.argv[1], sys.argv[2])
-for project in db.table("Project").all():
-    print("Scouting ", project['projectId'])
-    fetch_all(project)
+    try:
+        os.makedirs("Report Output")
+    except:
+        pass
+    try:
+        list_projects(project_or_org='project' if args.project else 'organization',
+                      specifier=args.project if args.project else args.organization)
+    except (HttpAccessTokenRefreshError, ApplicationDefaultCredentialsError):
+        import config
+        list_projects(project_or_org='project' if args.project else 'organization',
+                      specifier=args.project if args.project else args.organization)
+
+    for project in db.table("Project").all():
+        print("Scouting ", project['projectId'])
+        fetch_all(project)
+
+
+if __name__ == "__main__":
+    main()
